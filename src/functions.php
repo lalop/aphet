6e8f3fc359a97269c90eb2c943b4da2959c92353
Samuel Laulhau <sam@kribl.com>
2013-12-11 17:09:24 +0100

    fix tom much replace

--- src/functions.php
+++ src/functions.php
@@ -11,13 +11,13 @@ function aphet_init( \Aphet\Manager $manager)
 function aphet_url( $app_path, $name = null )
 {
     global $aphets_manager;
-    return $aphets_manager->computeAphetsUrl( $app_path, $name );
+    return $aphets_manager->computeAssetsUrl( $app_path, $name );
 }
 
 function aphet_htmlHelper($app_path, $html, $name = null )
 {
     if(!is_array($app_path)) $app_path = array($app_path);
-    $aphets_url = aphets_url( $app_path, $name );
+    $aphets_url = aphet_url( $app_path, $name );
     
     return implode('',array_map(function($path) use($html){
         return sprintf($html,$path);
@@ -27,10 +27,10 @@ function aphet_htmlHelper($app_path, $html, $name = null )
 
 function aphet_link( $aphet_url, $name = null )
 {
-    return aphets_htmlHelper( $aphet_url, "<link rel='stylesheet' href='%s' >\n", $name );
+    return aphet_htmlHelper( $aphet_url, "<link rel='stylesheet' href='%s' >\n", $name );
 }
    
 function aphet_script($aphet_url, $name = null )
 {
-    return aphets_htmlHelper($aphet_url, "<script src='%s'></script>\n", $name);
+    return aphet_htmlHelper($aphet_url, "<script src='%s'></script>\n", $name);
 }   
